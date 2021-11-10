d96af0da1c79ae8f758ebdef68119c2b96f545d8
aziza <azizafad8@gmail.com>
2021-11-10 12:30:41 +0200

    piza add feature

--- resources/views/pizza/create.blade.php
+++ resources/views/pizza/create.blade.php
@@ -13,9 +13,19 @@
                         <a href="" class="list-group-item list-group-item-action">Create</a>
                         
                         </ul>
-
                 </div>
             </div>
+            @if (count($errors) > 0)
+            <div class="card mt-5">
+                <div class="card-body">
+                    <div class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                           <p> {{ $error }}<p>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            @endif
         </div>
         <div class="col-md-8">
             <div class="card">
@@ -23,18 +33,7 @@
                 @if (count($errors) > 0)
                 <div class="card mt-5">
                     <div class="card-body">
-                        
-                        @if (count($errors) > 0)
-                        <div class="card mt-5">
-                            <div class="card-body">
-                                <div class="alert alert-danger">
-                                    @foreach ($errors->all() as $error)
-                                       <p> {{ $error }}<p>
-                                    @endforeach
-                                </div>
-                            </div>
-                        </div>
-                        @endif
+                   
                         
                     </div>
                 </div>
@@ -52,18 +51,22 @@
                     <div class="form-inline">
                         
                         <label>pizza price($)</label>
-                        <input type="number"  name="small pizza price" class="form-control" placeholder="small pizza price">
-                        <input type="number"  name="medium  pizza price"class="form-control" placeholder="medium pizza price">
-                        <input type="number"  name="large pizza price"class="form-control" placeholder="large pizza price">
+                        <input type="number" name="small_pizza_price" class="form-control"
+                        placeholder="small pizza price">
+                    <input type="number" name="medium_pizza_price" class="form-control"
+                        placeholder="medium pizza price">
+                    <input type="number" name="large_pizza_price" class="form-control"
+                        placeholder="large pizza price">
                      </div> 
                      <div class="form-group">
                         <label for="description">Category</label>
-                        <select class="form-control" name="Category">
+                        <select class="form-control" name="category">
                             <option value=""></option>
-                            <option value="vegetarian">vegetarian pizza</option>
-                            <option value="nonvegetarian">Non vegetarian pizza</option>
-                            <option value="traditional">Traditional pizza</option>
-                         </select>
+                            <option value="vegetarian">Vegetarian Pizza</option>
+                            <option value="nonvegetarian">Non vegetarian Pizza</option>
+                            <option value="traditional">Traditional Pizza</option>
+                        </select>
+                     </div>
                          <div class="form-group">
                             <label>Image</label>
                             <input type="file" name="image" class="form-control" name="image">
