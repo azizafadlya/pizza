06c949590c8df0bc7951f9a2f7aa27ea555579bb
aziza <azizafad8@gmail.com>
2021-11-13 13:39:45 +0200

    admin orders dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,10 +55,30 @@
                                 </li>
                             @endif
                         @else
+                       
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                       
+
                             <li class="nav-item dropdown">
+                                
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                    {{ Auth::user()->name }}   
                                 </a>
+                                
+                                <li class="nav-item"> <a class="nav-link"
+                                 href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                          document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+                        </li>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,7 @@ Route::post('/pizza/store', [App\Http\Controllers\PizzaController::class, 'store
 Route::get('/pizza/{id}/edit', [App\Http\Controllers\PizzaController::class, 'edit'])->name('pizza.edit');
 Route::put('/pizza/{id}/update', [App\Http\Controllers\PizzaController::class, 'update'])->name('pizza.update');
 Route::delete('/pizza/{id}/delete', [App\Http\Controllers\PizzaController::class, 'destroy'])->name('pizza.destroy');
+Route::get('/user/order', [App\Http\Controllers\UserOrderController::class, 'index'])->name('user.order');
+Route::post('/order/{id}/status', [App\Http\Controllers\UserOrderController::class, 'changeStatus'])->name('order.status');
+
 });
